0cc1c4620f72567bb0e3b8fe56079e4fd74b965c
ju310 <ju31094@gmail.com>
2015-01-05 20:59:45 +0100

    Cesar doesn't work
    cesar hat doch nicht so funktioniert wie gedacht, lag nur an dem
    testaufruf. habe jetzt eine funktion geschrieben, die einzelne chars mit
    cäsar verschlüsselt, da die auch bei vigenere ganz sinnvoll sein könnte.
    cesarc und cesard funktionieren noch nicht, isUpperCase, isLowerCase,
    singlecesarc schon, müssen aber noch kommentiert werden
    
    Lg katharina

--- codingmachine.c
+++ codingmachine.c
@@ -2,18 +2,61 @@
 #include <stdlib.h>
 #include <strings.h>
 
-/* function cesarc
+int IsUpperCase(char letter){
+    if (('A' <= letter)&&(letter <= 'Z')){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int IsLowerCase(char letter){
+    if (('a' <= letter)&&(letter <= 'z')){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+char cesarsinglec(char clear, char key){
+
+    int shift = key-'A'; printf("shift %d\n", shift);
+    int letternumber;
+
+    if (IsUpperCase(clear)){
+        letternumber = clear-'A';
+    }
+    if (IsLowerCase(clear)){
+        letternumber = clear-'a';
+    }
+    if ((IsUpperCase(clear) == 0)&&(IsLowerCase(clear) == 0)){
+        letternumber = 0;
+    }
+
+    if (shift+letternumber<26){
+        return clear+shift;
+    }
+    else{
+        return clear+shift-26;
+    }
+}
+
+char* cesarc(char* cleartext, int textlength, char key){
+/* function cesarc *******************************************************************************
+**
 ** input:
 **      char* cleartext: includes the text that has to be coded
 **      int textlength: length of the cleartext
 **      char key: letter that is used as a key letter for cesar, key shall be an uppercase letter
 ** output:
-**      char* including the coded text
+**      char* includes the coded text
 ** functionality:
-**      codes the cleartext by shifting all letters by key-'A',
+**      codes the cleartext by shifting all letters by key-'A' forwards,
 **      which means that A is coded to the key letter.
-**     */
-char* cesarc(char* cleartext, int textlength, char key){
+**
+*************************************************************************************************/
 
     /*local variables:
     *   count: int, counting varibale used for loop
@@ -37,14 +80,77 @@ char* cesarc(char* cleartext, int textlength, char key){
     return code;
 }
 
+char* cesard(char* codetext, int textlength, char key){
+/* function cesard *******************************************************************************
+**
+** input:
+**      char* codetext: includes the text that has to be decoded
+**      int textlength: length of the codetext
+**      char key: letter that is used as a key letter for cesar, key shall be an uppercase letter
+** output:
+**      char* includes the cleartext
+** functionality:
+**      decodes the codetext by shifting all letters backwards by key-'A',
+**      which means that A is coded to the key letter.
+**
+*************************************************************************************************/
+
+    /*local variables:
+    *   count: int, counting varibale used for loop
+    *   shift: int, shift needed for cesar decoding
+    *   code: char*, contains decoded text that is returned */
+    int count;
+    int shift = key-'A';
+    char* cleartext = (char*) malloc(textlength*sizeof(char));
+
+    /* shifts the codetext-letters shift chars backwards until the end of the alphabet is reached,
+    *  then starts again 26 letters before*/
+    for(count = 0; count < textlength; count++){
+        if (count+shift<26){
+            *(cleartext+count) = (char) *(codetext+count)-shift;
+        }
+        else{
+            *(cleartext+count) = (char) *(codetext+count)-shift+26;
+        }
+    }
+
+    return cleartext;
+}
+
+/* function vigenerec *******************************************************************************
+**
+** input:
+**      char* cleartext: includes the text that has to be coded
+**      int textlength: length of the cleartext
+**      char* keyword: key word for vigenere, shall be just uppercase letters
+**      int keylength: length of keyword
+** output:
+**      char* includes the codetext
+** functionality:
+**      codes the cleartext by shifting all letters backwards by keyword[i]-'A'
+**      with i=place in array mod keylength. uses cesarc therefore.
+**
+*************************************************************************************************/
+/* not done yet char* vigenerec(char* cleartext, int textlength, char* keyword, int keylength){
+
+    char* codetext;
+    int count;
+
+    for(count=0; count<textlength; count++){
+        codetext=cesard(cleartext, 1, *(keyword));
+    }
+
+    return codetext;
+}*/
+
 int main(){
 
-    char key='D';
-    char* text; char* code;
+    char key, clear, coded;
+    /*char* text; char* code; char* code2;
     int i;
 
-    for (i=0;i<26;i++){
-        *(text+i)='a'+i;
+    for (i=25;i>=0;i--){
+        *(text+i)='A'+i;
         printf("%c", *(text+i));
     }
     printf("\n");
@@ -53,6 +159,18 @@ int main(){
     for (i=0;i<26;i++){
         printf("%c", *(code+i));
     }
+
+    printf("\n");
+    code2=cesard(code, 26, key);
+
+    for (i=0;i<26;i++){
+        printf("%c", *(code2+i));
+    }*/
+    printf("Enter clear letter, a space and then the key letter, please:\n");
+    scanf("%c %c", &clear, &key);
+
+    coded=cesarsinglec(clear, key);
+    printf("\nclear:%c %d key:%c %d coded:%c %d\n", clear, clear, key, key, coded, coded);
 
     return 0;
 }
